ee951036dadfc9e8a1d65a78a5f17d982aa83163
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-07-25 14:57:28 +0800

    leetCode RelativeClass understood

--- test/src/main/java/tw/tsunglin/leetcode0725/RelativeClass.java
+++ test/src/main/java/tw/tsunglin/leetcode0725/RelativeClass.java
@@ -1,40 +1,41 @@
 package tw.tsunglin.leetcode0725;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class RelativeClass {
 	public String[] findRelativeRanks(int[] nums) {
 		String[] result = new String[nums.length];
-        int max = 0;
-        for (int i : nums) {
-            if (i > max) max = i;
+        int max=0;
+        for(int i=0; i<nums.length; i++) {
+        	if(nums[i]>max) {
+        		max=nums[i];
+        	}
         }
-        int[] hash = new int[max + 1];
-        for (int i = 0; i < nums.length; i++) {
-            hash[nums[i]] = i + 1;
+        
+        int[] hash = new int[max+1];
+        
+        for(int i=0; i<nums.length; i++) {
+        	hash[nums[i]]=i+1;
         }
-        int place = 1;
-        for (int i = hash.length - 1; i >= 0; i--) {
-            if (hash[i] != 0) {
-                if (place == 1) {
-                    result[hash[i] - 1] = "Gold Medal";
-                } else if (place == 2) {
-                    result[hash[i] - 1] = "Silver Medal";
-                } else if (place == 3) {
-                    result[hash[i] - 1] = "Bronze Medal";
-                } else {
-                    result[hash[i] - 1] = String.valueOf(place);
-                }
-                place++;
-            }
+        int place=1;
+        for(int i=hash.length-1; i>=0; i--) {
+        	if(hash[i]!=0) {
+        		if(place==1) {
+        			result[hash[i]-1]="gold";
+        		}else if(place==2) {
+        			result[hash[i]-1]="silver";
+        		}else if(place==3) {
+        			result[hash[i]-1]="bronze";
+        		}else {
+        			result[hash[i]-1]=String.valueOf(place);
+        		}
+        		place++;
+        	}
+        	
         }
         return result;
     }
 	public static void main(String[] args) {
 		RelativeClass test = new RelativeClass();
-		int[] nums = {5,4,3,2,1};
+		int[] nums = {4,5,3,2,1};
 		String[] ans = test.findRelativeRanks(nums);
 		
 		for(int i=0; i<ans.length; i++) {
