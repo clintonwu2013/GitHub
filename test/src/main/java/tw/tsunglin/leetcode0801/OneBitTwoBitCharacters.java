0d0f930b300176ae6704e20b52f0ad66d73abad1
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-02 09:36:30 +0800

    commit ibit2bitCharacters

--- test/src/main/java/tw/tsunglin/leetcode0801/OneBitTwoBitCharacters.java
+++ test/src/main/java/tw/tsunglin/leetcode0801/OneBitTwoBitCharacters.java
@@ -2,12 +2,15 @@ package tw.tsunglin.leetcode0801;
 
 public class OneBitTwoBitCharacters {
 	public static boolean isOneBitCharacter(int[] bits) {
-        int i = bits.length - 2;
-        while (i >= 0 && bits[i] > 0) i--;
-        return (bits.length - i) % 2 == 0;
+        int i = bits.length-2;
+        while(i>=0 && bits[i]>0) {
+        	i--;
+        }
+        boolean ans = (bits.length-i)%2 ==0;
+        return ans;
     }
 	public static void main(String[] arg) {
-		int[] bits = {1, 1,  0};
+		int[] bits = {0,1, 1,1,  0};
 		boolean ans = OneBitTwoBitCharacters.isOneBitCharacter(bits);
 		System.out.println(ans);
 	}
