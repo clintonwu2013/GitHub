15d3fb75bf2be0a7824eb96194db118ee979f8e8
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-06 10:55:21 +0800

    commit leetcode

--- test/src/main/java/tw/tsunglin/leetcode0805/PositionsOfLargeGroups.java
+++ test/src/main/java/tw/tsunglin/leetcode0805/PositionsOfLargeGroups.java
@@ -1,16 +1,29 @@
 package tw.tsunglin.leetcode0805;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PositionsOfLargeGroups {
-	public List<List<Integer>> largeGroupPositions(String S) {
-		return null;
-
-	}
-
+	public static List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int i = 0;
+        int N = S.length(); 
+        for(int j=0; j<N; j++) {
+        	if(j==N-1 || S.toCharArray()[j]!=S.toCharArray()[j+1]) {
+        		if((j-i+1)>=3) {
+        			List<Integer> temp = new ArrayList<Integer>();
+        			temp.add(i);
+        			temp.add(j);
+        			ans.add(temp);
+        		}
+        		i=j+1;
+        	}
+        }
+        return ans;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<List<Integer>> ans = PositionsOfLargeGroups.largeGroupPositions("abbcccccddddd");
+		System.out.println(ans);
 	}
 
 }
