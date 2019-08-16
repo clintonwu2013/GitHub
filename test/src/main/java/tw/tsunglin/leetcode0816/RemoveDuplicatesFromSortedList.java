d2a385f8f828a1d0e4c1b0d06cdfe6c740aca72c
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-16 13:27:16 +0800

    commit leetcode

--- test/src/main/java/tw/tsunglin/leetcode0816/RemoveDuplicatesFromSortedList.java
+++ test/src/main/java/tw/tsunglin/leetcode0816/RemoveDuplicatesFromSortedList.java
@@ -3,43 +3,20 @@ package tw.tsunglin.leetcode0816;
 public class RemoveDuplicatesFromSortedList {
 	 
 	public ListNode deleteDuplicates(ListNode head) {
-		ListNode previous = head;
-		ListNode next = previous.next;
+		ListNode current = head;
 		
-		ListNode ansHead = null;
-		
-		while(next!=null) {
-			if(previous.val != next.val) {
-				ansHead = new ListNode(previous.val);
-				ansHead.next = previous;
+		while(current!=null && current.next!=null) {
+			if(current.val == current.next.val) {
+				current.next = current.next.next;
+			}else {
+				current = current.next;
 			}
-			
-			previous = next;
-			next = previous.next;
-			
-			ansHead = ansHead.next;
 		}
-		ansHead = previous;
-		ansHead.val = previous.val;
-		return ansHead;
+		return head;
 
 	}
 	
-	public ListNode deleteDuplicates2(ListNode head) {
-	    ListNode current = head;
-	    while (current != null && current.next != null) {
-	        if (current.next.val == current.val) {
-	            current.next = current.next.next;
-	        } else {
-	            current = current.next;
-	        }
-	    }
-	    return head;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 	
 }
 
