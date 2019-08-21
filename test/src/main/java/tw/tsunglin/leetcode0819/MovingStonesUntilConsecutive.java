81a3f633549c16dc13d4fcbb4f51c323b12efccc
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-21 10:55:52 +0800

    commit leetcode

--- test/src/main/java/tw/tsunglin/leetcode0819/MovingStonesUntilConsecutive.java
+++ test/src/main/java/tw/tsunglin/leetcode0819/MovingStonesUntilConsecutive.java
@@ -18,6 +18,9 @@ public class MovingStonesUntilConsecutive {
 		}else if(array[2]-array[1]==1 && array[1]-array[0]==1) {
 			ans[0]=0;
 			ans[1]=0;
+		}else if(array[2]-array[1]==1 || array[1]-array[0]==1) {
+			ans[0]=1;
+			ans[1]=array[2]-array[0]-2;
 		}else {
 			ans[0]=2;
 			ans[1]=array[2]-array[0]-2;
@@ -26,7 +29,7 @@ public class MovingStonesUntilConsecutive {
         
     }
 	public static void main(String[] args) {
-		int[] ans = MovingStonesUntilConsecutive.numMovesStones(4, 8, 1);
+		int[] ans = MovingStonesUntilConsecutive.numMovesStones(1,2,5);
 		System.out.println(ans[0]);
 		System.out.println(ans[1]);
 
