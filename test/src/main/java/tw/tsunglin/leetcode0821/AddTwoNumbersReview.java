dbeb536ef5ce6acc25bad00a0c5452d30495f3e2
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-23 10:17:03 +0800

    commit leetcode

---
+++ test/src/main/java/tw/tsunglin/leetcode0821/AddTwoNumbersReview.java
@@ -0,0 +1,46 @@
+package tw.tsunglin.leetcode0821;
+
+public class AddTwoNumbersReview {
+
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummyHead = new ListNode(0);
+		ListNode  curr = dummyHead;
+		int carry = 0;
+		while(l1!=null || l2!=null) {
+			int x = (l1 != null)? l1.val:0;
+			int y = (l2 != null)? l2.val:0;
+			
+//			int sum = x+y+carry;
+			carry = (x+y+carry)/10;
+			
+			curr.next = new ListNode(carry);
+			curr = curr.next;
+			
+			if(l1!=null) {
+				l1 = l1.next;
+			}
+			if(l2!=null) {
+				l2 = l2.next;
+			}
+			
+		}
+		if(carry>0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
