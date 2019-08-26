5b8b6143961ef2b5d47967eb573dba951338534f
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-08-26 15:54:35 +0800

    commit calculator two stack algorithm

--- test/src/main/java/tw/tsunglin/leetcode0826/BasicCalculator.java
+++ test/src/main/java/tw/tsunglin/leetcode0826/BasicCalculator.java
@@ -1,46 +1,59 @@
 package tw.tsunglin.leetcode0826;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class BasicCalculator {
 
-	public int calculate(String s) {
-		 
-		Stack<Integer> stk = new Stack<Integer>();
-		// sign为1，可以考虑第一个数字没有符号的情况
-		int sign = 1;
-		int res = 0;
-		for (int i = 0; i < s.length(); i++) {
-			char ch = s.charAt(i);
-			if (Character.isDigit(ch)) {
-				int curSum = s.charAt(i) - '0';
-				// 由于之前用了++i<s.length(),导致错误，
-				// 原因是导致此if结束，又进去for循环，加了两次i
-				// 使得结果出错.使用在while循环里，
-				//如果不符合要求，不会自增，也就不会跳过符号位
-				while (i + 1 < s.length() 
-						&& Character.isDigit(s.charAt(i + 1)))
-					curSum = curSum * 10 + s.charAt(++i) - '0';
-				// res等于符号位乘以本次结果和之前的res之和
-				res += sign * curSum;
-			}else if (ch == '+') {
-				sign = 1;
-			}else if (ch == '-') {
-				sign = -1;
-			}else if (ch == '(') {// 遇到左括号时将res及括号之前的运算符压栈
-				// 因res存入栈中，需要将res置为0，取出的时候从栈取res计算
-				stk.push(res);
-				res = 0;// 注意
-				// sign置为1，已把sign放入栈中，重置
-				stk.push(sign);// 注意
-				sign = 1;
-			} else if (ch == ')') {
-				res = stk.pop() * res + stk.pop();
-				// sign = 1;
-			}
-		}
-		return res;
+	public  Double calculate(String s) {
+	    String[] str = s.split("\\s+");
+	    Queue<String> q = new LinkedList<String>();
+	    q.addAll(Arrays.asList(str));
+	    Stack<String> ops = new Stack<String>();
+	    Stack<Double> vals = new Stack<Double>();
+	    while (!q.isEmpty()) { // Read token, push if operator.
+	        String token = q.poll();
+	        if (token.equals("(")) {
+	        } else if (token.equals("+")) {
+	            ops.push(token);
+	        } else if (token.equals("-")) {
+	            ops.push(token);
+	        } else if (token.equals("*")) {
+	            ops.push(token);
+	        } else if (token.equals("/")) {
+	            ops.push(token);
+	        } else if (token.equals("sqrt")) {
+	            ops.push(token);
+	        } else if (token.equals(")")) { // Pop, evaluate, and push result if token is ")".
+	            // Replace the top exp with it' result.
+	            double v = vals.pop();
+	            String op = ops.pop();
+	            if (op.equals("+")) {
+	                v = vals.pop() + v;
+	            } else if (op.equals("-")) {
+	                v = vals.pop() - v;
+	            } else if (op.equals("*")) {
+	                v = vals.pop() * v;
+	            } else if (op.equals("/")) {
+	                v = vals.pop() / v;
+	            } else if (op.equals("sqrt")) {
+	                v = Math.sqrt(v);
+	            }
+	            vals.push(v);
+	        } else {
+	            // Token not operator or paren: push double value.
+	            vals.push(Double.parseDouble(token));
+	        }
+	    }
+	    return (vals.pop());
 	}
-	 
+
+	public static void main(String[] args) {
+		BasicCalculator calculator = new BasicCalculator();
+		Double ans = calculator.calculate("( ( 2 + 1 ) + 9 )");
+		System.out.println(ans);
+	}
 
 }
