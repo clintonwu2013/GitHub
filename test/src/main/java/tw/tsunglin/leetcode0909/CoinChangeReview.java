75b0bec142af3b941ee3ed281c3f869bbf2aa5f8
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-09-09 16:35:51 +0800

    commit leetcode

---
+++ test/src/main/java/tw/tsunglin/leetcode0909/CoinChangeReview.java
@@ -0,0 +1,28 @@
+package tw.tsunglin.leetcode0909;
+
+import java.util.Arrays;
+
+public class CoinChangeReview {
+	public static int coinChange(int[] coins, int amount) {
+		int max = amount + 1;   
+		int[] dp = new int[amount+1];
+		Arrays.fill(dp, max);
+		dp[0] = 0;
+		for(int i=1; i<=amount; i++) {
+			for(int j=0; j<coins.length; j++) {
+				if(i >= coins[j]) {
+					dp[i]= Math.min(dp[i], dp[i-coins[j]]+1);
+				}
+				
+			}
+		}
+        
+        return dp[amount] > amount ? -1:dp[amount];
+    }
+	public static void main(String[] args) {
+		int ans = CoinChange.coinChange(new int[] {1, 2, 5}, 11);
+		System.out.println(ans);
+
+	}
+
+}
