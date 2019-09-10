3ff594d91e515e13b37b3ead269ef5be6cc8f019
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-09-10 17:48:05 +0800

    COMMIT LEETCODE

---
+++ test/src/main/java/tw/tsunglin/leetcode0910/BinarySearchTreeReview.java
@@ -0,0 +1,145 @@
+package tw.tsunglin.leetcode0910;
+
+public class BinarySearchTreeReview {
+	public static  Node root;
+	public BinarySearchTreeReview(){
+		this.root = null;
+	}
+	
+	public boolean find(int key) {
+		Node current = root;
+		while(current != null) {
+			if(current.data == key) {
+				return true;
+			}else if(current.data > key){
+				current = current.left;
+			}else if(current.data < key){
+				current = current.right;
+			}
+		}
+		return false;
+		
+	}
+	public void insert(int id){
+		Node newNode = new Node(id);
+		if(root == null) {
+			root = newNode;
+			return;
+		}
+		Node parent = null;
+		Node current = root;
+		while(true) {
+			if(id < current.data) {
+				parent = current;
+				current = current.left;
+				if(current == null) {
+					parent.left = newNode;
+					return;
+				}
+			}else {
+				parent = current;
+				current = current.right;
+				if(current == null) {
+					parent.right = newNode;
+					return;
+				}
+			}
+		}
+		
+		
+	}
+	
+	public boolean delete(int id){
+		
+		if(root == null) {
+			return false;
+		}
+		
+		Node parent = root;
+		Node current = root;
+		boolean isLeftChild = false;
+		
+		while( current.data != id) {
+			parent = current;
+			if(id < current.data) {
+				isLeftChild = true;
+				current = current.left;
+			}else{
+				isLeftChild = false;
+				current = current.right;
+			}
+			if(current ==null){
+				return false;
+			}
+		}
+		
+		if(current.left == null && current.right == null) {
+			if(current == root) {
+				root = null;
+			}
+			if(isLeftChild) {
+				parent.left = null;
+			}else {
+				parent.right = null;
+			}
+			
+			return true;
+		}
+		else if(current.right==null){
+			if(current == root) {
+				root = current.left;
+			}else if (isLeftChild) {
+				parent.left = current.left;
+			}else {
+				parent.right = current.left;
+			}
+		}
+		else if(current.left==null){
+			if(current == root) {
+				root = current.right;
+			}else if (isLeftChild) {
+				parent.left = current.right;
+			}else {
+				parent.right = current.right;
+			}
+		}
+		else if(current.left!=null && current.right!=null){
+			Node successor = getSuccessor(current);
+			if(current == root) {
+				root = successor;
+			}else if(isLeftChild) {
+				parent.left = successor;
+			}else {
+				parent.right = successor;
+			}
+			successor.left = current.left;
+		}
+		
+		
+		return true;
+	}
+	
+	public Node getSuccessor(Node deleleNode){
+		Node successsor =null;
+		Node successsorParent =null;
+		Node current = deleleNode.right;
+		while(current!=null){
+			successsorParent = successsor;
+			successsor = current;
+			current = current.left;
+		}
+		//check if successor has the right child, it cannot have left child for sure
+		// if it does have the right child, add it to the left of successorParent.
+//		successsorParent
+		if(successsor!=deleleNode.right){
+			successsorParent.left = successsor.right;
+			successsor.right = deleleNode.right;
+		}
+		return successsor;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
