114d86d0d337e60af27776c14b4b80cc3e04bb32
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-09-16 12:52:53 +0800

    commit leetcode

---
+++ test/src/main/java/tw/tsunglin/leetcode0911/LongestHarmoniousSubsequence.java
@@ -0,0 +1,24 @@
+package tw.tsunglin.leetcode0911;
+
+import java.util.HashMap;
+
+public class LongestHarmoniousSubsequence {
+	public int findLHS(int[] nums) {
+        HashMap < Integer, Integer > map = new HashMap <Integer, Integer > ();
+        int res = 0;
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key: map.keySet()) {
+            if (map.containsKey(key + 1))
+                res = Math.max(res, map.get(key) + map.get(key + 1));
+        }
+        return res;
+    }
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
