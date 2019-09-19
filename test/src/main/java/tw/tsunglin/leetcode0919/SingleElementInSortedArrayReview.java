53d0e92777987c7420f47cc9466fc6411973defe
6503 <6503@TV6503NB.tradevanad.intranet.local>
2019-09-19 16:12:00 +0800

    commit leetcode

---
+++ test/src/main/java/tw/tsunglin/leetcode0919/SingleElementInSortedArrayReview.java
@@ -0,0 +1,24 @@
+package tw.tsunglin.leetcode0919;
+
+public class SingleElementInSortedArrayReview {
+	public int singleNonDuplicate(int[] nums) {
+		int low = 0;
+		int high = nums.length-1;
+		int mid = 0;
+		while( low!= high) {
+			mid = low + (high-low)/2;
+			if(nums[mid] != nums[mid+1]) {
+				low = mid;
+			}else {
+				high = mid;
+			}
+			
+		}
+		return mid;
+	}
+	public static void main(String[] args) {
+		SingleElementInSortedArray test = new SingleElementInSortedArray();
+		int ans = test.singleNonDuplicate(new int[] {1,3,3,7,7,10,10,11,11});
+		System.out.println(ans);
+	}
+}
